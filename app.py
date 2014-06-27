15a63c10a8e1c10e4accd50517039cae1ffaa518
scorpio7500@gmail.com <pw183@prolog.cs.rutgers.edu>
2014-06-26 23:27:50 -0400

    done

--- app.py
+++ app.py
@@ -3,9 +3,17 @@
 from flask import Flask, requests
 
 app = Flask(__name__)
+data = None
 
-@app.route('/submitdata')
-	
+@app.route('/submitdata', methods=['POST'])
+def submit():
+    global data 
+    data = request.args.get('data')
+
+@app.route('/getdata',methods=['GET'])
+def send():
+    global data
+    return data
 
 if __name__ == '__main__':
 	app.run()
