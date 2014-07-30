17c461bee21921691b3e431bf72bea780569f5cb
Timothy Yong <tyong_23@hotmail.com>
2014-07-30 16:20:14 -0400

    Changed flask server to receive json requests

--- iplink/app.py
+++ iplink/app.py
@@ -1,29 +1,19 @@
 from flask import Flask, request
 
 app = Flask(__name__)
-data = None
+data = {}
 
 @app.route('/')
 def display():
 	return str(data)
 
-@app.route('/submitdata', methods=['POST'])
+@app.route('/submitdata', methods = ['POST'])
 def submit():
-	global data
-<<<<<<< HEAD:app.py
-	print "TRYING TO RECEIVE: " + str(request.args)	
-	data = request.args
-=======
-	print "TRYING TO RECEIVE: " + str(request.data)	
-	data = request.form['data']
->>>>>>> efeacf0b5865f35193e117f9e51d4a91b10924c2:iplink/app.py
-	print "RECEIVED: " + str(data)
-	return "RECEIVED: " + str(request)
+	data = request.json
 
-@app.route('/getdata',methods=['GET'])
+@app.route('/getdata', methods = ['GET'])
 def send():
-	global data
-	return data
+	data = request.json
 
 if __name__ == '__main__':
 	app.run('127.0.0.1', port=3000, debug=True)
