981f80d88f938ec49296e9a01b2ea543b8886d70
Timothy Yong <tyong_23@hotmail.com>
2014-07-30 16:15:10 -0400

    Changed iplink to incorporate json format

--- iplink/iplink.py
+++ iplink/iplink.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import requests
+import json
 
 # Mushed <sender.py> and <receiver.py> together
 
@@ -7,11 +8,11 @@ class iplink():
     def __init__(self, url = "http://127.0.0.1:3000"):
         self.url = url
 
-    def post(self, data, extraurl = "/submitdata"):
-        url = self.url + extraurl
-		response = requests.post(url, data)
+    def post(self, message, url = "/submitdata"):
+        url = self.url + url
+        response = requests.post(url, data = json.dumps(message), headers = {"content-type": "application/json"})
 
-    def get(self, extraurl = "/getdata"):
-        url = self.url + extraurl
-		response = requests.get(url)
-		return response.text
+    def get(self, url = "/getdata"):
+        url = self.url + url
+        response = requests.get(url)
+        return response.json()
